949ee47a908b681f6559f8153484b0760326cf00
PilotFreak <vovasoldatenkov@gmail.com>
2026-05-28 20:34:30 +0300

    Module 2 complete.

--- src/main/java/com/example/helpdesk/controller/HelpdeskApplication.java
+++ src/main/java/com/example/helpdesk/controller/HelpdeskApplication.java
@@ -1,4 +1,4 @@
-package com.example.helpdesk;
+package com.example.helpdesk.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
